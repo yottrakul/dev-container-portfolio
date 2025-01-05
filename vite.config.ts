3781881e8ffda70e48b3abd242c0ac4a38457332
Yottrakul Suwannasri <zxc734@gmail.com>
2025-01-05 12:15:10 +0000

    vite + svelte setup

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  server: {
+    port: 3000,
+    host: true
+  }
+})
